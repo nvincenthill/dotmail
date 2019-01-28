1ecb0fa3cc3197539dcb147e97fde81c3422de08
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-27 18:05:07 -0800

    Add body parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./../routes');
 
 const app = express();
@@ -10,6 +11,7 @@ app.set('port', process.env.PORT || 8080);
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
 
+app.use(bodyParser.json());
 app.use('/api', routes);
 
 module.exports = app;
