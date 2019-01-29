0fe631b831c5759133a716f279608895d708af29
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-29 11:27:42 -0800

    Refactor send email protocol

--- controllers/send.js
+++ controllers/send.js
@@ -1,48 +1,53 @@
-const mjmlUtils = require('mjml-utils');
-const path = require('path');
+const Handlebars = require('handlebars');
 const transporter = require('./transporter');
 const { createMJML, sendError, transpileMJML } = require('./helpers');
 
 module.exports = {
   post: (req, res) => {
-    const { email, subject, message } = req.body;
+    const {
+      name, email, subject, message,
+    } = req.body;
     createMJML((err) => {
       if (err) {
         sendError(err, res, 'Failed to create MJML');
       }
-
-      transpileMJML((err) => {
+      transpileMJML((err, html) => {
         if (err) {
           sendError(err, res, 'Failed to transpile MJML');
         }
 
-        const pathToHtmlEmailTemplate = path.join(__dirname, '../data/template.html');
+        // Use Handlebars.js to inject template email HTML
+        const template = Handlebars.compile(html);
 
-        mjmlUtils.sendmail.config({
-          fromAddress: process.env.EMAIL,
-          transport: transporter,
-        });
+        const data = {
+          name,
+          city: 'Oakland, CA',
+          kids: [{ name: 'Jimmy', age: '12' }, { name: 'Sally', age: '4' }],
+        };
 
-        console.log('sending email...');
+        const hydrated = template(data);
 
-        mjmlUtils
-          .sendmail({
-            to: email,
-            subject,
-            text: message,
-            template: pathToHtmlEmailTemplate,
-            // The same data you would pass to #inject()
-            data: { exampleVariable: 'BANANAS' },
-          })
-          .then(() => {
+        console.log('Sending email...');
+
+        const mail = {
+          from: process.env.EMAIL,
+          to: email,
+          subject,
+          html: hydrated,
+          text: message,
+        };
+
+        transporter.sendMail(mail, (err, data) => {
+          if (err) {
+            console.log(err);
+            sendError(err, res, 'Failed to send email', data);
+          } else {
             console.log('Successfully sent email!');
             res.json({
               message: 'Successfully sent email!',
             });
-          })
-          .catch((error) => {
-            sendError(error, res, 'Failed to send email');
-          });
+          }
+        });
       });
     });
   },
