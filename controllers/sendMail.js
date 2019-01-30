9accc3b499f5067cd0e4cb2759766bf4921dc1c2
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-01-30 10:54:16 -0800

    Rename to sendMail

---
+++ controllers/sendMail.js
@@ -0,0 +1,47 @@
+const transporter = require('./transporter');
+const {
+  createMJML, sendError, transpileMJML, injectVariablesIntoTemplate,
+} = require('./helpers');
+
+module.exports = {
+  post: (req, res) => {
+    // TODO: Refactor to accept a template object with different shape
+    const {
+      name, email, subject, message,
+    } = req.body;
+    createMJML((err) => {
+      if (err) {
+        sendError(err, res, 'Failed to create MJML');
+      }
+      transpileMJML('data/template.mjml', (error, html) => {
+        if (error) {
+          sendError(error, res, 'Failed to transpile MJML');
+        }
+
+        const variables = {
+          name,
+          city: 'Oakland, CA',
+          kids: [{ name: 'Jimmy', age: '12' }, { name: 'Sally', age: '4' }],
+        };
+
+        const mail = {
+          from: process.env.EMAIL,
+          to: email,
+          subject,
+          html: injectVariablesIntoTemplate(html, variables),
+          text: message,
+        };
+
+        transporter.sendMail(mail, (transportError, data) => {
+          if (transportError) {
+            sendError(transportError, res, 'Failed to send email', data);
+          } else {
+            res.json({
+              message: 'Successfully sent email!',
+            });
+          }
+        });
+      });
+    });
+  },
+};
