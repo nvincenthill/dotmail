9f0fb912f3d4cb3c5948f6959ccb8ba897f7bc33
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-02 15:21:52 -0800

    Refactor EmailSender to decouple from server res

--- controllers/EmailSender.js
+++ controllers/EmailSender.js
@@ -1,22 +1,10 @@
-const { sendError } = require('./helpers');
-
 class EmailSender {
   constructor(transporter) {
     this.transporter = transporter;
   }
 
-  send(mail, res) {
-    this.transporter
-      .sendMail(mail)
-      .then((data) => {
-        res.json({
-          message: 'Successfully sent email!',
-          data,
-        });
-      })
-      .catch((error) => {
-        sendError(error, res, 'Failed to send email');
-      });
+  send(mail) {
+    this.transporter.sendMail(mail);
   }
 }
 
