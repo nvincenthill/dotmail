23a0cf60f3d3e92fdb4d8e711c52fcebeba01e5f
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-03 00:32:21 -0800

    Change variable naming

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -7,12 +7,12 @@ const EmailCreator = require('./EmailCreator');
 
 module.exports = {
   sendMail: (req, res) => {
-    const { template, recipients } = req.body;
+    const { form, recipients } = req.body;
     const transpiler = new MJMLTranspiler();
     const emailCreator = new EmailCreator(process.env.EMAIL);
     const emailSender = new EmailSender(transporter);
 
-    transpiler.transpile(`data/${template.name}.mjml`, (error, html) => {
+    transpiler.transpile(`data/${form.name}.mjml`, (error, html) => {
       if (error) {
         sendError(error, res, 'Failed to transpile MJML');
       }
@@ -25,9 +25,9 @@ module.exports = {
         };
         const mail = emailCreator.create(
           recipients[i].email,
-          template.subjectLine,
+          form.subjectLine,
           injectVariablesIntoTemplate(html, injections),
-          template.message,
+          form.message,
         );
         const transmission = emailSender.send(mail);
         transmissions.push(transmission);
