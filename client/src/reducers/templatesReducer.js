6d48369fd4a5208eb494e9041588a4b1c2512865
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-04 11:24:32 -0800

    Change shape template state

--- client/src/reducers/templatesReducer.js
+++ client/src/reducers/templatesReducer.js
@@ -1,41 +1,35 @@
 import { UPDATE_TEMPLATES } from '../actions/actionTypes';
 
-const initialState = {
-  templates: [
-    {
-      id: 1,
-      name: 'Thank You Email',
-      type: 'personal',
-      senderName: 'Nicholas Vincent-Hill',
-      subjectLine: 'Thank you!',
-      message: 'Hello world!',
-    },
-    {
-      id: 2,
-      name: 'Tech Checkup Follow-up',
-      type: 'technicalMentor',
-      senderName: 'Nicholas Vincent-Hill',
-      subjectLine: '[ACTION REQUIRED] Tech Check Follow Up',
-      message: 'Hello world!',
-    },
-    {
-      id: 3,
-      name: 'Attendance',
-      type: 'coordinator',
-      senderName: 'Nicholas Vincent-Hill',
-      subjectLine: 'Attendance 1/26',
-      message: 'Hello world!',
-    },
-    {
-      id: 4,
-      name: 'Example Template',
-      type: 'universal',
-      senderName: 'Nicholas Vincent-Hill',
-      subjectLine: 'Example Subject Line',
-      message: 'Hello world!',
-    },
-  ],
-};
+const initialState = [
+  {
+    id: 0,
+    name: 'Thank You Email',
+    type: 'personal',
+    subjectLine: 'Thank you!',
+    message: 'Hello world!',
+  },
+  {
+    id: 1,
+    name: 'Tech Checkup Follow-up',
+    type: 'technicalMentor',
+    subjectLine: '[ACTION REQUIRED] Tech Check Follow Up',
+    message: 'Tech Checkup example text',
+  },
+  {
+    id: 2,
+    name: 'Attendance',
+    type: 'coordinator',
+    subjectLine: 'Attendance 1/26',
+    message: 'Attendance example text',
+  },
+  {
+    id: 3,
+    name: 'Example Template',
+    type: 'universal',
+    subjectLine: 'Example Subject Line',
+    message: 'Example template example text',
+  },
+];
 
 export default function templatesReducer(state = initialState, action) {
   if (action.type === UPDATE_TEMPLATES) {
