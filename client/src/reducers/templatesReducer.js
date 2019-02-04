27a2e5bd8d284fc804d09fa52d2170df0335ecf4
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-04 14:01:55 -0800

    Abstract templates to JSON file

--- client/src/reducers/templatesReducer.js
+++ client/src/reducers/templatesReducer.js
@@ -1,35 +1,7 @@
 import { UPDATE_TEMPLATES } from '../actions/actionTypes';
+import templatesData from '../store/templates.json';
 
-const initialState = [
-  {
-    id: 0,
-    name: 'Thank You Email',
-    type: 'personal',
-    subjectLine: 'Thank you!',
-    message: 'Hello world!',
-  },
-  {
-    id: 1,
-    name: 'Tech Checkup Follow-up',
-    type: 'technicalMentor',
-    subjectLine: '[ACTION REQUIRED] Tech Check Follow Up',
-    message: 'Tech Checkup example text',
-  },
-  {
-    id: 2,
-    name: 'Attendance',
-    type: 'coordinator',
-    subjectLine: 'Attendance 1/26',
-    message: 'Attendance example text',
-  },
-  {
-    id: 3,
-    name: 'Example Template',
-    type: 'universal',
-    subjectLine: 'Example Subject Line',
-    message: 'Example template example text',
-  },
-];
+const initialState = templatesData.templates;
 
 export default function templatesReducer(state = initialState, action) {
   if (action.type === UPDATE_TEMPLATES) {
