17cf652ef216504548be39d1b292e31be33a8b69
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-04 11:22:59 -0800

    Add updateDisplayedTemplate action

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -4,6 +4,10 @@ export function updateField(payload) {
   return { type: types.UPDATE_FIELD, field: payload.field, value: payload.value };
 }
 
+export function updateDisplayedTemplate(form) {
+  return { type: types.UPDATE_DISPLAYED_TEMPLATE, form };
+}
+
 export function updateUser(payload) {
   return { type: types.UPDATE_USER, name: payload.name, email: payload.email };
 }
