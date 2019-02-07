e0eb5c2b318561ff4ce2d0621e6f3b1341fff56d
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-06 16:08:55 -0800

    Add env support and polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((acc, el) => {
+  acc[`process.env.${el}`] = JSON.stringify(env[el]);
+  return acc;
+}, {});
+
 module.exports = {
-  entry: `${__dirname}/client/src/index.jsx`,
+  entry: ['@babel/polyfill', `${__dirname}/client/src/index.jsx`],
   output: {
     filename: 'bundle.js',
     path: `${__dirname}/client/dist`,
@@ -25,4 +34,5 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [new webpack.DefinePlugin(envKeys)],
 };
