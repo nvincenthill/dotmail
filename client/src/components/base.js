075a60d96ea846b22bd3dd0ff8d003660adc0959
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-06 17:33:36 -0800

    Use firestore as database

--- client/src/components/base.js
+++ client/src/components/base.js
@@ -1,13 +1,16 @@
 import Rebase from 're-base';
 import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 });
 
-const base = Rebase.createClass(firebaseApp.database());
+const db = firebase.firestore();
+const base = Rebase.createClass(db);
 
-export { firebaseApp };
+export { firebaseApp, db };
 export default base;
