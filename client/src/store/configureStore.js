20088e49c87e4473840efd9bf0ada80e74117327
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-06 17:34:51 -0800

    Bugfix compose issue in store

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
   return createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
