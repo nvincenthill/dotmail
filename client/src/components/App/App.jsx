ed52cca659d327d2c319be73981c40f6ad8098aa
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-07 10:44:06 -0800

    Move auth and firestore logic to app

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import base, { firebaseApp, db } from '../base';
+
 // import Response from './Response';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -11,16 +15,102 @@ const AppStyles = styled.div`
   padding: 1rem 1.5rem;
   background: #eee;
 `;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uid: null,
+      userAuthenticated: false,
+      userData: [],
+      isLoading: true,
+    };
 
-const App = () => (
-  <React.Fragment>
-    <AppStyles>
-      <Header />
-      {/* <Response /> */}
-      <FormContainer />
-    </AppStyles>
-    <Footer />
-  </React.Fragment>
-);
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+    this.writeUserData = this.writeUserData.bind(this);
+  }
+
+  componentDidMount() {
+    this.ref = base.bindCollection('userData', {
+      context: this,
+      state: 'userData',
+      withRefs: true,
+      then() {
+        this.setState({ isLoading: false });
+      },
+    });
+  }
+
+  authenticate(provider) {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  }
+
+  async authHandler(authData) {
+    // ! user is now authenticated and but not trusted
+    // TODO: change firestore database read/write rules
+    this.setState({
+      uid: authData.user.uid,
+      userAuthenticated: true,
+    });
+    if (!authData.additionalUserInfo.isNewUser) {
+      // TODO: fetch data from db
+      db.collection('usersData')
+        .doc(`${authData.user.uid}`)
+        .get()
+        .then((docRef) => {
+          console.log('Document written with ID: ', docRef.id);
+        })
+        .catch((error) => {
+          console.error('Error adding document: ', error);
+        });
+    } else {
+      // TODO: handle new user
+      this.writeUserData();
+    }
+
+    base.addToCollection('userData', {
+      hello: 'world',
+    });
+  }
+
+  async logOut() {
+    await firebase.auth().signOut();
+    this.setState({
+      uid: null,
+      userAuthenticated: false,
+    });
+  }
+
+  writeUserData() {
+    const { uid } = this.state;
+    db.collection('users')
+      .add({
+        uid,
+      })
+      .then((docRef) => {
+        console.log('Document written with ID: ', docRef.id);
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <AppStyles>
+          <Header />
+          {/* <Response /> */}
+          <FormContainer />
+        </AppStyles>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,90 +1,21 @@
 import React from 'react';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import base, { firebaseApp, db } from '../base';
-
 class Header extends React.Component {
   constructor() {
     super();
-    this.state = {
-      uid: null,
-      userAuthenticated: false,
-      userData: null,
-      isLoading: true,
-      userData: [],
-    };
+    this.state = {};
     this.handleClick = this.handleClick.bind(this);
-    this.authenticate = this.authenticate.bind(this);
-    this.authHandler = this.authHandler.bind(this);
-    this.writeUserData = this.writeUserData.bind(this);
   }
 
-  componentDidMount() {
-    this.ref = base.bindCollection('userData', {
-      context: this,
-      state: 'userData',
-      withRefs: true,
-      then() {
-        this.setState({ isLoading: false });
-      },
-    });
-  }
-
-  handleClick() {
-    this.authenticate('Google');
-  }
-
-  authenticate(provider) {
-    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-    firebaseApp
-      .auth()
-      .signInWithPopup(authProvider)
-      .then(this.authHandler);
-  }
-
-  async authHandler(authData) {
-    // ! user is now authenticated and but not trusted
-    // TODO: change firestore database read/write rules
-    this.setState({
-      uid: authData.user.uid,
-      userAuthenticated: true,
-    });
-
-    const { uid } = this.state;
-
-    if (!authData.additionalUserInfo.isNewUser) {
-      // TODO: fetch data from db
-    } else {
-      // TODO: handle new user
-      this.writeUserData();
+  handleClick(e) {
+    if (e.target.value === 'Log in') {
+      // TODO: Log in user
+      this.authenticate('Google');
     }
 
-    base.addToCollection('userData', {
-      hello: 'world',
-    });
-  }
-
-  async logOut() {
-    await firebase.auth().signOut();
-    this.setState({
-      uid: null,
-      userAuthenticated: false,
-    });
-  }
-
-  writeUserData() {
-    const { uid } = this.state;
-    db.collection('users')
-      .add({
-        uid,
-      })
-      .then((docRef) => {
-        console.log('Document written with ID: ', docRef.id);
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      });
+    if (e.target.value === 'Log out') {
+      // TODO: Log out user
+    }
   }
 
   render() {
@@ -95,7 +26,10 @@ class Header extends React.Component {
           <button type="submit" onClick={e => this.handleClick(e)}>
             Log in
           </button>
-          <div>{`User Authenticated: ${this.state.userAuthenticated}`}</div>
+
+          <button type="submit" onClick={e => this.handleClick(e)}>
+            Log out
+          </button>
         </div>
       </div>
     );
