4055a9e1109857e9600bb6c79b413464fc7dd3c1
Fernando <ferdelamad@gmail.com>
2019-02-10 23:39:19 -0800

    Add recipient animation

--- client/src/components/Recipients/Recipient.jsx
+++ client/src/components/Recipients/Recipient.jsx
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { StyledBtn } from '../../elements';
 
 import { Below } from '../../utilities';
 
+const slideUp = keyframes`
+  0% {
+      opacity: 0;
+      transform: translateY(20px);
+  }
+  100% {
+      opacity: 1;
+      transform: translateY(0);
+  }
+`;
+
 const RecipientContainer = styled.div`
   margin: 1rem auto;
+  padding: 0.75rem;
   align-self: center;
   justify-content: center;
   display: grid;
-  max-width: 4rem;
+  box-shadow: ${({ theme }) => theme.elevations.med};
+  border: 1px solid ${({ theme }) => theme.colors.accentColor5};
+  animation: ${slideUp} 0.4s ease;
   grid-template-columns: 15rem 15rem;
   grid-template-areas:
-    "firstName lastName"
-    "preferred email"
-    "add       cancel";
+    'firstName lastName'
+    'preferred email'
+    'add       cancel';
   grid-gap: 0.5rem;
   ${Below.small`
     grid-template-columns: 15rem;
@@ -31,7 +45,11 @@ const RecipientContainer = styled.div`
 
 const Input = styled.input`
   grid-area: ${({ area }) => area};
-`
+  border: 0.01rem solid ${({ theme }) => theme.colors.accentColor5};
+  :focus {
+    box-shadow: 0 0 0.01rem ${({ theme }) => theme.colors.accentColor5};
+  }
+`;
 
 const GridElement = styled.div`
   grid-area: ${({ area }) => area};
@@ -71,12 +89,35 @@ class Recipient extends Component {
     } = this.state;
     return (
       <RecipientContainer>
-        <Input placeholder="Name" area="firstName" type="text" id="firstName" name="firstName" ref={firstName} />
-        <Input placeholder="Last Name" area="lastName"type="text" id="lastName" name="lastName" ref={lastName} />
-        <Input placeholder="Preferred" area="preferred" type="text" id="preferred" name="preferred" ref={preferred} />
-        <Input placeholder="E-mail" area="email" type="text" id="email" name="email" ref={email} />
+        <Input
+          placeholder="Name"
+          area="firstName"
+          type="text"
+          id="firstName"
+          name="firstName"
+          ref={firstName}
+        />
+        <Input
+          placeholder="Last Name"
+          area="lastName"
+          type="text"
+          id="lastName"
+          name="lastName"
+          ref={lastName}
+        />
+        <Input
+          placeholder="Preferred"
+          area="preferred"
+          type="text"
+          id="preferred"
+          name="preferred"
+          ref={preferred}
+        />
+        <Input placeholder="Email" area="email" type="text" id="email" name="email" ref={email} />
         <GridElement area="add">
-          <StyledBtn align="center" onClick={() => this.handleClick('ADD')}>Add</StyledBtn>
+          <StyledBtn align="center" onClick={() => this.handleClick('ADD')}>
+            Add
+          </StyledBtn>
         </GridElement>
         <GridElement area="cancel">
           <StyledBtn align="center" type="cancel" onClick={this.handleClick}>
