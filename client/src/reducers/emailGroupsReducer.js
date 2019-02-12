6d81cd0fd9d0a6ca8d60f7446eaf34a5bcf476e9
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-12 15:51:05 -0800

    Refactor initial state and add delete group method

--- client/src/reducers/emailGroupsReducer.js
+++ client/src/reducers/emailGroupsReducer.js
@@ -1,4 +1,4 @@
-import { ADD_EMAIL_GROUP } from '../actions/actionTypes';
+import { ADD_EMAIL_GROUP, DELETE_EMAIL_GROUPS } from '../actions/actionTypes';
 
 const initialState = [
   {
@@ -6,35 +6,14 @@ const initialState = [
     name: 'None',
     recipients: [],
   },
-  {
-    id: 1,
-    name: 'ExampleGroupName',
-    recipients: [
-      {
-        firstName: 'First recipient firstName',
-        lastName: 'First recipient lastName',
-        preferred: 'First recipient preferredName',
-        email: 'nvincenthill@gmail.com',
-      },
-      {
-        firstName: 'Second recipient firstName',
-        lastName: 'Second recipient lastName',
-        preferred: 'Second recipient preferredName',
-        email: 'nvincenthill@gmail.com',
-      },
-      {
-        firstName: 'Third recipient firstName',
-        lastName: 'Third recipient lastName',
-        preferred: 'Third recipient preferredName',
-        email: 'nvincenthill@gmail.com',
-      },
-    ],
-  },
 ];
 
 export default function emailGroupsReducer(state = initialState, action) {
   if (action.type === ADD_EMAIL_GROUP) {
-    return { ...state, ...action.group };
+    return [...state, action.group];
+  }
+  if (action.type === DELETE_EMAIL_GROUPS) {
+    return [];
   }
   return state;
 }
