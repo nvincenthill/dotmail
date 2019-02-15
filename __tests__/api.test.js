ca2c39e603589bde5600e833594c8c6f6399e71b
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-02-14 16:16:50 -0800

    Implement OPTIONS req handling and POST int test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,13 +1,31 @@
 const request = require('supertest');
 const app = require('../server/app');
 
-describe('GET', () => {
-  it('Should get a response given a good GET request', () => request(app)
-    .get('/api/')
-    .then((response) => {
-      expect(response.statusCode).toBe(200);
-    }));
+const examplePOSTbody = {
+  form: {
+    greeting: 'Hi',
+    id: 5,
+    injections: { title: 'Message from the sublime' },
+    message:
+      'Thank you so much for that thing you did for me. I really appreciate your help. Thanks again!',
+    name: 'Galvanize Letterhead',
+    salutation: 'Warm regards,',
+    subjectLine: 'Welcome toHR114!',
+    templateName: 'doesnotexist',
+    type: 'techmentor',
+  },
+  currentUser: { name: 'John Smith', email: 'example@gmail.com' },
+  recipients: [
+    {
+      firstName: 'Example',
+      lastName: 'Student1',
+      preferred: 'Example',
+      email: 'nvincenthill@gmail.com',
+    },
+  ],
+};
 
+describe('GET', () => {
   it('Should 404 a bad GET request', () => request(app)
     .get('/api/&(@$Y@(^@)#(3/8989-09')
     .then((response) => {
@@ -16,10 +34,17 @@ describe('GET', () => {
 });
 
 describe('POST', () => {
-  // it('should respond with status code 201', () => request(app)
-  //   .post('/api')
-  //   .send({})
-  //   .expect(201));
+  it('should respond with status code 200 to a valid POST request', () => request(app)
+    .post('/api/send')
+    .send(JSON.stringify(examplePOSTbody))
+    .expect(200));
+
+  it('should respond with a well formed error object to an invalid POST body', (done) => {
+    request(app)
+      .post('/api/send')
+      .send(JSON.stringify(examplePOSTbody))
+      .expect(200, { message: 'ERROR: Invalid sender or recipient(s)!', err: true }, done);
+  });
 });
 
 describe('PUT', () => {
@@ -43,7 +68,7 @@ describe('DELETE', () => {
 });
 
 describe('OPTIONS', () => {
-  it('should respond with status code 200', () => request(app)
+  it('should respond with status code 200 to a valid options request', () => request(app)
     .options('/api')
     .expect(200));
 });
--- server/app.js
+++ server/app.js
@@ -14,4 +14,14 @@ app.use(express.static('client/dist'));
 app.use(bodyParser.json());
 app.use('/api', routes);
 
+app.options('/*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With',
+  );
+  res.sendStatus(200);
+});
+
 module.exports = app;
