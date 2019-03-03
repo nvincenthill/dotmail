92f11083ee261654e478b87c4898c6ff217aa7f5
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2019-03-03 14:01:53 -0800

    Move redux dev tools to .env

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import async from '../middleware/async';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = process.env.REDUXDEVTOOLS || compose;
 
 export default function configureStore() {
   return createStore(rootReducer, composeEnhancer(applyMiddleware(async)));
